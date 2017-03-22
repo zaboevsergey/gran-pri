dea6a95da940f41ab8246b8520ccf71ff1ff90a3
zaboevsergey <zaboevsergey@yandex.ru>
2017-03-22 21:46:03 +0500

    Delete start.php

--- start.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once("database.php");
-require_once("models/starts.php");
-
-$link = db_connect();
-$start = get_start($link, $_GET['sid']);
-
-include ("views/starts/start.php");
-?>
